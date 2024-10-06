29d0cf3f76245be15ec1d6ad84e115e39b82a461
Mukhtar Abdulfatai <mukhtarabdulfatai@gmail.com>
2024-10-06 20:17:59 +0100

    cleanup

--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -1,12 +1,11 @@
 'use client'
-import React, { createContext, useState, useEffect } from 'react';
+import React, { createContext, useState } from 'react';
 
 export const GlobalStateContext = createContext();
 
 export const GlobalStateProvider = ({ children }) => {
     const [loading, setLoading] = useState(false)
  
-
     return (
         <GlobalStateContext.Provider value={{ loading, setLoading}}>
             {children}
